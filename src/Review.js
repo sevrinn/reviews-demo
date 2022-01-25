5b5bd38f1b46c4bd28c6c6b3c98b22abf45d8a67
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-25 07:46:05 -0800

    added react-icons

---
+++ src/Review.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
+const Review = () => {
+  return <div></div>
+}
+
+export default Review

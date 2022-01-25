acb8e9ff51454363cefb6527136e5ebf290efc4b
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-25 08:09:42 -0800

    added state and destructrured imported data

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,21 @@ import people from './data'
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <div></div>
+  //state is set to 0 initially, representing the index of our people array
+  const [index, setIndex] = useState(0)
+  //destructure people of a certain index
+  const { name, job, image, text } = people[index]
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+    </article>
+  )
 }
 
 export default Review
